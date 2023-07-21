7d4cf4ff1549c537c6eaf06b414788aab52c6451
bielsil27@gmail.com <bielsil27@gmail.com>
2023-07-21 14:16:21 -0300

    proximos passos

--- public/index.php
+++ public/index.php
@@ -3,4 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/routes.php';
 
-//falta terminar documentação e bloquear acesso direto a pags php
+//falta terminar documentação
+//resposta na documentação não vem indentada
+//bloquear acesso direto a pags php
+//certificar que é restful
