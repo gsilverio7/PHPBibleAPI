1e5b3a82574ce35d479652ccfada4ca70d2d30e3
bielsil27@gmail.com <bielsil27@gmail.com>
2023-07-21 14:11:26 -0300

    feat: finished basic api documentation

--- src/controllers/BibleController.php
+++ src/controllers/BibleController.php
@@ -7,7 +7,11 @@ use API\Utils\Response;
 class BibleController 
 {
     /**
-     * @OA\Info(title="Bible API", version="1.0")
+     * @OA\Info(
+     *  title="Bible API", 
+     *  description="RESTful Bible API with multiple language and version support. Created with PHP without any major framework.",
+     *  version="1.0"
+     * )
      */
 
     private $services;
@@ -19,8 +23,69 @@ class BibleController
     
     /**
      * @OA\Get(
-     *     path="/{lang}/{version}/{book}/{chapter}/{verses?}",
-     *     @OA\Response(response="200", description="An example resource")
+     *     path="/api/{lang}/{version}/{book}/{chapter}/{verses}",
+     *     summary="Get bible verses",
+     *     tags={"default"},
+     *     @OA\Parameter(
+     *         description="Bible Language",
+     *         in="path",
+     *         name="lang",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Parameter(
+     *         description="Bible Version",
+     *         in="path",
+     *         name="version",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Parameter(
+     *         description="Bible Book",
+     *         in="path",
+     *         name="book",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Parameter(
+     *         description="Bible Chapter",
+     *         in="path",
+     *         name="chapter",
+     *         required=true,
+     *         @OA\Schema(type="integer"),
+     *     ),
+     *     @OA\Parameter(
+     *         description="Bible Verses",
+     *         in="path",
+     *         name="verses",
+     *         required=false,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 example={
+     *                      "info": {
+     *                          "language": "Portuguese", 
+     *                          "version": "Almeida Atualizada", 
+     *                          "book": "Gênesis", 
+     *                          "chapter": 1
+     *                      }, 
+     *                      "verses": {
+     *                          "verse": 1, 
+     *                          "text": "No princípio criou Deus os céus e a terra."
+     *                      }
+     *                  }
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad params request"
+     *     )
      * )
      */
     public function getVerses(string $lang, string $version, string $book, int $chapter, string $verses = '')
@@ -36,6 +101,39 @@ class BibleController
         return Response::json($response, 400);
     }
     
+    /**
+     * @OA\Get(
+     *     path="/api/info",
+     *     summary="Get info about current languages and versions available.",
+     *     tags={"default"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 example={
+     *                     "pt": {
+     *                         "language": "Portuguese", 
+     *                             "versions": {
+     *                                 "aa": "Almeida Atualizada", 
+     *                                 "acf": "Almeida Corrigida Fiel", 
+     *                                 "nvi": "Nova Versão Internacional"
+     *                             }
+     *                     }, 
+     *                     "en" : {
+     *                          "language": "English", 
+     *                          "versions": {
+     *                                 "bbe": "Basic Bible English",
+     *                                 "kjv": "King James Version"
+     *                          }
+     *                     }
+     *                 }
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function showInfo()
     {
         $response = [];
