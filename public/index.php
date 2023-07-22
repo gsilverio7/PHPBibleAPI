38ece1623b1b95353a8db8b9914f8c65752cda0d
bielsil27@gmail.com <bielsil27@gmail.com>
2023-07-22 14:23:01 -0300

    fix: tests not working

--- public/index.php
+++ public/index.php
@@ -3,6 +3,5 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/routes.php';
 
-//resposta na documentação não vem indentada
 //bloquear acesso direto a pags php
 //certificar que é restful
--- src/utils/Response.php
+++ src/utils/Response.php
@@ -6,8 +6,11 @@ class Response
 {
     public static function json($response, int $statusCode = 200): string
     {
+        if (! headers_sent()) {
+            header('Content-Type: application/json');
+        }
+
         http_response_code($statusCode);       
-        header('Content-Type: application/json');
         return json_encode($response, JSON_UNESCAPED_UNICODE);
     }
 }
