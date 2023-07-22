e9276b33d1ad3ebe93223ed1eab370d6cb7462b2
bielsil27@gmail.com <bielsil27@gmail.com>
2023-07-22 13:18:38 -0300

    feat: finishing documentation

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/routes.php';
 
-//falta terminar documentação
 //resposta na documentação não vem indentada
 //bloquear acesso direto a pags php
 //certificar que é restful
