e3b1a46fd5ff99f63fe931de5fc3ed9f63e642d9
bielsil27@gmail.com <bielsil27@gmail.com>
2023-07-22 15:23:24 -0300

    swagger ui via cdn

--- api/documentation.php
+++ api/documentation.php
@@ -1,17 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>Bible API</title>
-    <link rel="stylesheet" type="text/css" href="swagger/swagger-ui.css" />
-    <link rel="stylesheet" type="text/css" href="swagger/index.css" />
-    <link rel="icon" type="image/png" href="swagger/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="swagger/favicon-16x16.png" sizes="16x16" />
-  </head>
-  <body>
-    <div id="swagger-ui"></div>
-    <script src="swagger/swagger-ui-bundle.js" charset="UTF-8"> </script>
-    <script src="swagger/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
-    <script src="swagger/swagger-initializer.js" charset="UTF-8"> </script>
-  </body>
+<html>
+
+<head>
+    <title>Docs</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.17.0/swagger-ui.css">
+</head>
+
+<body>
+    <div id="ui-wrapper-new" data-spec="{{spec}}">
+        Loading....
+    </div>
+</body>
+<script src="https://unpkg.com/swagger-ui-dist@3.23.1/swagger-ui-bundle.js"></script>
+<script>
+    var swaggerUIOptions = {
+      url: "openapi.json",
+      dom_id: '#ui-wrapper-new', // Determine what element to load swagger ui
+      docExpansion: 'list',
+      deepLinking: true, // Enables dynamic deep linking for tags and operations
+      filter: true,
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
+      ],
+      plugins: [
+        SwaggerUIBundle.plugins.DownloadUrl
+      ],
+    }
+
+    var ui = SwaggerUIBundle(swaggerUIOptions)
+
+    /** Export to window for use in custom js */
+    window.ui = ui
+</script>
+
 </html>
