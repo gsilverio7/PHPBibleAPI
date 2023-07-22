2ff5309ef6c22b22bcc221863dd220416dc1114c
bielsil27@gmail.com <bielsil27@gmail.com>
2023-07-22 13:27:46 -0300

    fix: header for responses add header informing that response is in JSON format

--- src/utils/Response.php
+++ src/utils/Response.php
@@ -7,6 +7,7 @@ class Response
     public static function json($response, int $statusCode = 200): string
     {
         http_response_code($statusCode);       
+        header('Content-Type: application/json');
         return json_encode($response, JSON_UNESCAPED_UNICODE);
     }
 }
